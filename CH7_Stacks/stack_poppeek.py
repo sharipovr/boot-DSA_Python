03cd3fc7eece35379767fff5ca3e4b926fc780fc
sharipovr <sharipov.rustem@gmail.com>
2025-12-05 22:15:49 -0600

    Started with CH7_Stack

---
+++ CH7_Stacks/stack_poppeek.py
@@ -0,0 +1,20 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def size(self):
+        return len(self.items)
+
+    def peek(self):
+        if self.size() == 0:
+            return None
+        return self.items[-1]
+
+    def pop(self):
+        if self.size() == 0:
+            return None
+        res = self.items.pop()
+        return res
