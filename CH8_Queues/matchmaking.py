bfbc290f1b5d116181f3ab37e52a32734ce88bcb
sharipovr <sharipov.rustem@gmail.com>
2025-12-05 23:34:59 -0600

    Completed CH8_Queues

---
+++ CH8_Queues/matchmaking.py
@@ -0,0 +1,13 @@
+from queue import Queue
+
+
+def matchmake(queue, user):
+    if user[1] == 'leave':
+        queue.search_and_remove(user[0])
+    if user[1] == 'join':
+        queue.push(user[0]) 
+    if queue.size() >= 4:
+        user1 = queue.pop()
+        user2 = queue.pop()
+        return f"{user1} matched {user2}!"   
+    return "No match found"
